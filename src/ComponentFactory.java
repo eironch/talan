e98af0451829972c8e557c3ca817c5fc121fd35d
ficuno <eldritchgallows@gmail.com>
2023-12-15 02:12:27 +0800

    create component toolset

---
+++ src/ComponentFactory.java
@@ -0,0 +1,32 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class ComponentFactory {
+    public void createContainer(JPanel panel, FlowLayout layout, int width, int height){
+        panel.setLayout(layout);
+        panel.setPreferredSize(new Dimension(width, height));
+    }
+
+    public void createContainer(Container container, FlowLayout layout, int width, int height){
+        container.setLayout(layout);
+        container.setPreferredSize(new Dimension(width, height));
+    }
+
+    public void createContainer(JPanel panel, FlowLayout layout){
+        panel.setLayout(layout);
+    }
+
+    public void createContainer(Container container, FlowLayout layout){
+        container.setLayout(layout);
+    }
+
+    public void createContainer(JPanel panel, GridLayout layout, int width, int height){
+        panel.setLayout(layout);
+        panel.setPreferredSize(new Dimension(width, height));
+    }
+
+    public void createContainer(Container container, GridLayout layout, int width, int height){
+        container.setLayout(layout);
+        container.setPreferredSize(new Dimension(width, height));
+    }
+}
