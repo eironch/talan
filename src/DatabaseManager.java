f3c5e5548d53af49b0b7c1d31e13ed9a64ade877
ficuno <eldritchgallows@gmail.com>
2023-12-20 12:34:15 +0800

    add creation of database table add password hashing

--- src/DatabaseManager.java
+++ src/DatabaseManager.java
@@ -1,21 +1,33 @@
 import java.sql.*;
-
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
 public class DatabaseManager {
     public static void main(String[] args) throws SQLException {
 
-        String url = "jdbc:mysql://localhost:3306/note_taking_app"; //URL of database to be connected
-        Connection myConn = DriverManager.getConnection(url, "root", ""); //Connect to database (Requires JDBC) [Default username:root, pw empty]
+        String url = "jdbc:mysql://localhost:3306/note_taking_app";
+        Connection myConn = DriverManager.getConnection(url, "root", "");
 
-        Statement statement= myConn.createStatement(); //Create a Statement object to run SQL statements on DB
+        Statement statement= myConn.createStatement();
 
-        String query0="CREATE TABLE IF NOT EXISTS NOTES ("+  // Initial query to create table if not already present in DB
-                "date DATE PRIMARY KEY," +
-                "time TIMESTAMP NOT NULL," +
-                "note text NOT NULL" +
+        String createUsersTable="CREATE TABLE IF NOT EXISTS USERS ("+
+                "id INT NOT NULL," +
+                "username VARCHAR(16) NOT NULL," +
+                "password TEXT NOT NULL" +
                 ")";
 
-        statement.executeUpdate(query0); //executeUpdate(statement) is used to run DDL (e.g. CREATE) or DML (e.g INSERT) commands
+        statement.executeUpdate(createUsersTable);
 
+        String createTasksTable="CREATE TABLE IF NOT EXISTS TASKS ("+
+                "note TEXT NOT NULL," +
+                "time TIMESTAMP NOT NULL," +
+                "date DATE NOT NULL," +
+                "status VARCHAR(7) NOT NULL," +
+                "user_id INT NOT NULL," +
+                "FOREIGN KEY (user_id) REFERENCES USERS (id)" +
+                ")";
+
+        statement.executeUpdate(createTasksTable);
 
 //        String query1 = "INSERT INTO STUDENT VALUES (?, ?, ?)";
 //        PreparedStatement preStat = myConn.prepareStatement(query1); //PreparedStatement is a subclass of Statement that supports data substitution and can execute a statement multiple times
@@ -26,17 +38,29 @@ public class DatabaseManager {
 //        preStat.executeUpdate(); //Executing the statement using executeUpdate()
 
 
-        String query2 = "SELECT * FROM NOTES;";
+//        String query2 = "SELECT * FROM NOTES;";
 
-        ResultSet result = statement.executeQuery(query2); //executeQuery(statement) is used to run DQL command (i.e. SELECT) and returns a ResultSet object
+//        ResultSet result = statement.executeQuery(query2); //executeQuery(statement) is used to run DQL command (i.e. SELECT) and returns a ResultSet object
 
-        while(result.next()) { //Now iterating over the ResultSet object to print the results of the query. next() returns false after all rows exhausted, else returns true
-            int regno = result.getInt("regno"); //Getters extract corresponding data from column names
-            String name = result.getString("name");
-            String branch = result.getString("branch");
-            System.out.println("Name - " + name);
-            System.out.println("Branch - " + branch);
-            System.out.println("Registration number - " + regno);
+//        while(result.next()) { //Now iterating over the ResultSet object to print the results of the query. next() returns false after all rows exhausted, else returns true
+//            int regno = result.getInt("regno"); //Getters extract corresponding data from column names
+//            String name = result.getString("name");
+//            String branch = result.getString("branch");
+//            System.out.println("Name - " + name);
+//            System.out.println("Branch - " + branch);
+//            System.out.println("Registration number - " + regno);
+//        }
+    }
+
+    private static String hashPassword(String password) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] hash = digest.digest(password.getBytes());
+
+            return Base64.getEncoder().encodeToString(hash);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return null;
         }
     }
 }
