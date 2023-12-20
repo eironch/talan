eec6ec5b0ddbc8225f365495606d230d3e9f6fac
ficuno <eldritchgallows@gmail.com>
2023-12-20 15:08:39 +0800

    move code to

--- src/ComponentFactory.java
+++ src/ComponentFactory.java
@@ -2,6 +2,8 @@ import javax.swing.*;
 import java.awt.*;
 
 public class ComponentFactory {
+    AssetHandler asset = new AssetHandler();
+    ComponentToolbox tool = new ComponentToolbox();
     public void createContainer(JPanel panel, FlowLayout layout, int width, int height){
         panel.setLayout(layout);
         panel.setPreferredSize(new Dimension(width, height));
@@ -37,5 +39,73 @@ public class ComponentFactory {
     public void createContainer(Container container, BorderLayout layout, int width, int height){
         container.setLayout(layout);
         container.setPreferredSize(new Dimension(width, height));
+    }
+
+    public Container createDivider(int marginN, int marginS){
+        Container dividerSectionContainer = new Container();
+        Container dividerContainer = new Container();
+        Container dividerAssetContainer = new Container();
+        JPanel divider = new JPanel();
+        JLabel dividerLeft = new JLabel();
+        JLabel dividerRight = new JLabel();
+
+        createContainer(dividerSectionContainer,
+                new GridLayout(), Main.WIDTH, 15 + marginS);
+        createContainer(dividerContainer,
+                new FlowLayout(FlowLayout.CENTER), Main.WIDTH,15);
+        createContainer(dividerAssetContainer,
+                new FlowLayout(FlowLayout.CENTER, 0,0), Main.WIDTH, 4);
+
+        dividerLeft.setIcon(asset.dividerLeftIcon);
+        dividerLeft.setPreferredSize(new Dimension(asset.dividerLeftIcon.getIconWidth(),8));
+        dividerLeft.setVerticalAlignment(SwingConstants.TOP);
+
+        dividerRight.setIcon(asset.dividerRightIcon);
+        dividerRight.setPreferredSize(new Dimension(asset.dividerRightIcon.getIconWidth(),8));
+        dividerRight.setVerticalAlignment(SwingConstants.TOP);
+
+        divider.setBackground(tool.toColor(Main.BROWN));
+        divider.setPreferredSize(new Dimension(Main.WIDTH - (Main.WIDTH/4),8));
+
+        dividerAssetContainer.add(dividerLeft);
+        dividerAssetContainer.add(divider);
+        dividerAssetContainer.add(dividerRight);
+
+        dividerContainer.add(dividerAssetContainer);
+        dividerSectionContainer.add(dividerContainer);
+
+        return dividerSectionContainer;
+    }
+
+    public Container createDivider(){
+        Container dividerSectionContainer = new Container();
+        Container dividerContainer = new Container();
+        JPanel divider = new JPanel();
+        JLabel dividerLeft = new JLabel();
+        JLabel dividerRight = new JLabel();
+
+        createContainer(dividerSectionContainer,
+                new FlowLayout(), Main.WIDTH,15);
+        createContainer(dividerContainer,
+                new FlowLayout(FlowLayout.CENTER, 0,0), Main.WIDTH, 4);
+
+        dividerLeft.setIcon(asset.dividerLeftIcon);
+        dividerLeft.setPreferredSize(new Dimension(asset.dividerLeftIcon.getIconWidth(),8));
+        dividerLeft.setVerticalAlignment(SwingConstants.TOP);
+
+        dividerRight.setIcon(asset.dividerRightIcon);
+        dividerRight.setPreferredSize(new Dimension(asset.dividerRightIcon.getIconWidth(),8));
+        dividerRight.setVerticalAlignment(SwingConstants.TOP);
+
+        divider.setBackground(tool.toColor(Main.BROWN));
+        divider.setPreferredSize(new Dimension(Main.WIDTH - (Main.WIDTH/4),8));
+
+        dividerContainer.add(dividerLeft);
+        dividerContainer.add(divider);
+        dividerContainer.add(dividerRight);
+
+        dividerSectionContainer.add(dividerContainer);
+
+        return dividerSectionContainer;
     }
 }
