fa0ec4a2493be061f7e7e9eefdc5fd65fa3e9652
ficuno <eldritchgallows@gmail.com>
2023-12-21 19:51:06 +0800

    update code

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
     final static int YELLOW = 0xFFD34F;
     final static int LIGHT_YELLOW = 0xffe082;
     final static int BROWN = 0x584d2c;
+    final static int LIGHT_BROWN = 0x847443;
     final static int BLACK = 0x000000;
     final static int WHITE = 0xFFFFFF;
     final static int WIDTH = 540;
