23b5dba69f8bc889e042becca317165a996693da
ficuno <eldritchgallows@gmail.com>
2023-12-27 10:39:52 +0800

    add save asset

--- src/AssetHandler.java
+++ src/AssetHandler.java
@@ -22,6 +22,10 @@ public class AssetHandler {
             resizeIcon(new ImageIcon("assets/dividerRightIcon.png"), 30, 4);
     ImageIcon doneIcon =
             resizeIcon(new ImageIcon("assets/doneIcon.png"), 30, 30);
+    ImageIcon saveIcon =
+            resizeIcon(new ImageIcon("assets/saveIcon.png"), 35, 35);
+    ImageIcon savedIcon =
+            resizeIcon(new ImageIcon("assets/savedIcon.png"), 35, 35);
 
     private static ImageIcon resizeIcon(ImageIcon icon, int width, int height) {
         return new ImageIcon(icon.getImage().getScaledInstance(width, height, Image.SCALE_SMOOTH));
