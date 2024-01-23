b409a7691eb660a0fc023605d2d8d0ea98481b25
ficuno <eldritchgallows@gmail.com>
2024-01-23 20:23:18 +0800

    move jframe

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class Main {
+    static JFrame frame = new JFrame();
+    static AssetFactory a = new AssetFactory();
     final static int YELLOW = 0xFFD34F;
     final static int LIGHT_YELLOW = 0xffe082;
     final static int BROWN = 0x584d2c;
@@ -9,9 +12,20 @@ public class Main {
     final static int WHITE = 0xFFFFFF;
     final static int WIDTH = 540;
     final static int HEIGHT = 960;
+    final static String COLOR_LIGHT_BROWN = a.toColor(Main.LIGHT_BROWN).toString();
+
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> {
-            new MainView().setVisible(true);
-        });
+        frame.setTitle("Talan");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(Main.WIDTH, Main.HEIGHT);
+        frame.setBounds((1920 / 2)- (WIDTH / 2), (1080 / 2)- (HEIGHT / 2), WIDTH, HEIGHT);
+        frame.setResizable(false);
+        frame.setLayout(new BorderLayout());
+        frame.getContentPane().setBackground(a.toColor(Main.LIGHT_YELLOW));
+        frame.setIconImage(a.icon.getImage());
+
+        SwingUtilities.invokeLater(() -> new MainView(frame));
+
+        frame.setVisible(true);
     }
 }
